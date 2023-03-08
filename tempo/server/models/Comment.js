bc4f8d469caee520db4f0955d03c0ebb1184758f
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-03-08 11:53:38 -0700

    CommentSchema

---
+++ tempo/server/models/Comment.js
@@ -0,0 +1,16 @@
+import { Schema } from "mongoose";
+import { basicStringType, defaultSchemaOptions } from "../db/Constants";
+
+export const CommentSchema = new Schema ({
+  body: basicStringType,
+  creatorId: {type: Schema.Types.ObjectId, required: true, ref: 'Account'},
+  playlistId: {type: Schema.Types.ObjectId, required: true, ref: 'Playlist'}, 
+
+}, defaultSchemaOptions)
+
+CommentSchema.virtual('creator', {
+  ref: 'Account',
+  localField: 'creatorId',
+  foreignField: '_id',
+  justOne: true
+})
