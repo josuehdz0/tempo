a037b4b081a740d92fa9129e154edaf5fa1fcc07
Josue Hernandez <josuehdz0@gmail.com>
2023-03-08 12:30:31 -0700

    Ligin styling

--- tempo.client/src/components/Login.vue
+++ tempo.client/src/components/Login.vue
@@ -5,7 +5,7 @@
     </button>
     <div v-else>
       <div class="dropdown dropstart my-2 my-lg-0">
-        <div type="button" class="bg-dark border-0 selectable no-select" data-bs-toggle="dropdown" aria-expanded="false">
+        <div type="button" class="bg-light border-0 selectable no-select" data-bs-toggle="dropdown" aria-expanded="false">
           <div v-if="account.picture || user.picture">
             <img :src="account.picture || user.picture" alt="account photo" height="40" class="rounded" />
           </div>
