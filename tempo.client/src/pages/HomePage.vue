f7a0bcc92c368c526ce519921efc4c92bc255913
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-03-08 14:59:10 -0700

    commented out reference for playlist card

--- tempo.client/src/pages/HomePage.vue
+++ tempo.client/src/pages/HomePage.vue
@@ -3,6 +3,24 @@
     <div class="row justify-content-center ">
 
       <!-- NOTE This is where the playlistcard Component goes -->
+      <!-- <div class="col-10 col-md-5 mt-4 cardbg">
+        <div class="row text-light py-3 px-1">
+          <div class="col-3">
+            picture
+          </div>
+          <div class="col-7">
+            <h2>
+              Playlist Name
+            </h2>
+            <div>
+              By (playlist creator)
+            </div>
+          </div>
+          <div class="col-2 d-flex justify-content-center align-items-center">
+            <i class="mdi mdi-heart heart"></i>
+          </div>
+        </div>
+      </div> -->
       <PlaylistCard />
 
 
@@ -14,11 +32,12 @@
 </template>
 
 <script>
+
 import PlaylistCard from "../components/PlaylistCard.vue";
 
 export default {
   setup() {
-    return {};
+    return {}
   },
   components: { PlaylistCard }
 }
