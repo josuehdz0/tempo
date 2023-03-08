7a894fa78df669d6ea1ad0734341b8306836a879
Scott Tippetts <wstippetts+github@gmail.com>
2023-03-08 14:00:50 -0700

    about page contains problem statement.

--- tempo.client/src/pages/AboutPage.vue
+++ tempo.client/src/pages/AboutPage.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is the about page</h1>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-8">
+          <b>
+
+            <p>
+              Have you ever been on a run listening to some sick beats killing your previous best when out of nowhere Josh
+              Groben
+              comes on and kills your vibe!? Today sharks we present to you Tempo!
+            </p>
+            <p>
+              Tempo allows people to stack their playlists according to the pace they want to keep using complex
+              algorithms that
+              calculate a person’s stride per minute to reach a desired mile time and match that to a list of songs with
+              the
+              optimum tempo for each individual.
+            </p>
+          </b>
+
+
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
