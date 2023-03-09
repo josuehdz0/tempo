0e9a0c3263a9ccfa94c39bff936bd38cacd3d35b
Scott Tippetts <wstippetts+github@gmail.com>
2023-03-09 09:46:24 -0700

    BE post comment & get comment by playlist

--- tempo/server/controllers/CommentsController.js
+++ tempo/server/controllers/CommentsController.js
@@ -1,5 +1,25 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
+import { commentsService } from "../services/CommentsService.js";
 import BaseController from "../utils/BaseController";
 
 export class CommentsController extends BaseController {
+  constructor() {
+    super('api/comments')
+    this.router
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.postComment)
+  }
+  async postComment(req, res, next) {
+    try {
+      const postData = req.body
+      postData.creatorId = req.userInfor.creatorId
+      const comment = await commentsService.postComment(postData)
+      return res.send(comment)
 
+    } catch (error) {
+      next(error)
+    }
+
+
+  }
 }
