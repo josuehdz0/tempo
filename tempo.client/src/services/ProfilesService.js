ac80073f9e3ed3e3ab7fe1dde8045ae0b1a01801
Scott Tippetts <wstippetts+github@gmail.com>
2023-03-09 11:33:21 -0700

    profile is editable

---
+++ tempo.client/src/services/ProfilesService.js
@@ -0,0 +1,29 @@
+import { AppState } from "../AppState.js"
+import { Profile } from "../models/Account.js"
+import { logger } from "../utils/Logger.js"
+import { api } from "./AxiosService.js"
+
+class ProfilesService {
+
+  async getProfileById(profileId) {
+    const res = await api.get('api/profiles/' + profileId)
+    logger.log('getting profile', res.data)
+    AppState.profile = new Profile(res.data)
+  }
+
+
+
+  async editProfile(profileData) {
+    const res = await api.put('/account', profileData)
+    logger.log(res.data, 'edit?')
+    AppState.account = res.data
+  }
+
+  clearProfile() {
+    AppState.profile = null
+  }
+
+
+
+}
+export const profilesService = new ProfilesService()
