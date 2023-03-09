ba1aaaba536da4a9f784d707360e5edbad61c823
Josue Hernandez <josuehdz0@gmail.com>
2023-03-09 10:33:43 -0700

    profile page updates

--- tempo.client/src/pages/ProfilePage.vue
+++ tempo.client/src/pages/ProfilePage.vue
@@ -1,42 +1,57 @@
 <template>
   <div class="container-fluid">
     <div class="row justify-content-center mt-4">
-      <div class="col-11 cardbg">
+      <div class="col-10 bigcardbg">
         <div class="row">
-          <div class="col-5 p-2 d-flex justify-content-center">
-            <img src="http://placekitten.com/110/110" alt="" class="profile-img rounded">
+          <div class="col-6 p-1 d-flex justify-content-center p-4">
+            <img :src="account.picture" :alt="account.name" class="profile-img ">
           </div>
-          <div class="col-7 p-2 d-flex flex-column align-items-center justify-content-center">
+          <div class="col-6 p-2  align-items-center text-light">
             <div>
-              <h5>
+              <h5 class="text-center">
                 Scotty
               </h5>
             </div>
             <div>
-              Total Playlists: 0
+              <b>
+                Total Playlists: 0
+
+              </b>
             </div>
             <div>
-              Favorite Genre: Ska
+              <b>
+
+                Favorite Genre: Ska
+              </b>
             </div>
             <div>
-              Average BPM: 125
+              <b>
+
+                Average BPM: 125
+              </b>
             </div>
           </div>
         </div>
       </div>
     </div>
     <div class="row mt-4">
-      <div class="col-12 d-flex justify-content-evenly">
-        <div class="btn text-dark filter-btn">
-          Saved Playlists
+      <div class="col-12 d-flex justify-content-evenly text-light">
+        <div class="btn text-light filter-btn">
+          <h3>
+
+            Saved Playlists
+          </h3>
         </div>
-        <div class="btn text-dark filter-btn">
-          My Playlists
+        <div class="btn text-light filter-btn">
+          <h3>
+            My Playlists
+
+          </h3>
         </div>
       </div>
     </div>
     <div class="row justify-content-center">
-      <div class="col-11 cardbg">
+      <div class="col-10 cardbg">
         <PlaylistCard />
       </div>
     </div>
@@ -45,11 +60,15 @@
 
 
 <script>
+import { computed } from "vue";
+import { AppState } from "../AppState.js";
 import PlaylistCard from "../components/PlaylistCard.vue";
 
 export default {
   setup() {
-    return {};
+    return {
+      account: computed(() => AppState.account)
+    };
   },
   components: { PlaylistCard }
 }
@@ -63,9 +82,17 @@ export default {
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 
+.bigcardbg {
+  background-color: #2b4141d4;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
 .filter-btn {
   font-size: 15pt;
 }
 
-.profile-img {}
+.profile-img {
+  border-radius: 10px;
+}
 </style>
