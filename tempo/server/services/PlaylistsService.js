dcda55fac0ab8fadd80ad030416d0fc7e9c1c4ec
rtuscany23 <122835790+rtuscany23@users.noreply.github.com>
2023-03-10 10:52:37 -0700

    modified createPlaylist()

--- tempo/server/services/PlaylistsService.js
+++ tempo/server/services/PlaylistsService.js
@@ -1,12 +1,32 @@
 import { dbContext } from "../db/DbContext.js"
 import { BadRequest } from "../utils/Errors.js"
+import { TrackModel } from "../models/TrackModel.js"
 
 class PlaylistsService {
+
   async createPlaylist(playlistData) {
-    const playlist = await dbContext.Playlists.create(playlistData)
-    await playlist.populate('creator')
-    return playlist
+    const playlist = await dbContext.Playlists.create({
+      name: playlistData.name,
+      tempo: playlistData.tempo,
+      genre: playlistData.genre,
+      runtime: playlistData.runtime,
+      creatorId: playlistData.creatorId,
+      tracks: this.convertToTrackModels(playlistData.tracks) // convert tracks to TrackModels
+    });
+    await playlist.populate('creator');
+    return playlist;
   }
+
+  convertToTrackModels(trackData) {
+    return trackData.map(data => new TrackModel(data));
+  }
+
+  // async createPlaylist(playlistData) {
+  //   const playlist = await dbContext.Playlists.create(playlistData)
+  //   await playlist.populate('creator')
+  //   return playlist
+  // }
+
   async getAllPlaylists() {
     const playlists = await dbContext.Playlists.find()
       .populate('creator')
