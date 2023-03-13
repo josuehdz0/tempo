a6e2d3a61489f23b7be33da680586ced35916f27
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-03-13 16:04:10 -0600

    track card css

--- tempo.client/src/components/TrackCard.vue
+++ tempo.client/src/components/TrackCard.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="row text-light py-3 px-1">
-    <div class="col-3">
-      {{ track.albumImg }}
-    </div>
-    <div class="col-7">
-      <h5>
-        {{ track.name }}
-      </h5>
-      <div>
-        By Artist
+  <div class="col-10 col-md-5 mt-4 songcardbg">
+    <div class="row text-light py-3 px-1">
+      <div class="col-3">
+        {{ track.albumImg }}
       </div>
-    </div>
-    <div class="col-2 d-flex justify-content-center align-items-center">
-      <i class="mdi mdi-spotify spotify"></i>
+      <div class="col-7">
+        <h5>
+          {{ track.name }}
+        </h5>
+        <div>
+          By Artist
+        </div>
+      </div>
+      <div class="col-2 d-flex justify-content-center align-items-center">
+        <i class="mdi mdi-spotify spotify"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -34,4 +36,10 @@ export default {
 </script>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.songcardbg {
+  background-color: #2b414169;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+</style>
