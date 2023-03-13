d9518182228ef25aa7a5d38ad3bcb806d20b165b
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-03-13 16:00:33 -0600

    track card format

--- tempo.client/src/components/TrackCard.vue
+++ tempo.client/src/components/TrackCard.vue
@@ -1,22 +1,18 @@
 <template>
-  <div class="row justify-content-center">
-    <div class="col-10 col-md-5 mt-4 songcardbg">
-      <div class="row text-light py-3 px-1">
-        <div class="col-3">
-          {{ track.albumImg }}
-        </div>
-        <div class="col-7">
-          <h5>
-            {{ track.name }}
-          </h5>
-          <div>
-            By Artist
-          </div>
-        </div>
-        <div class="col-2 d-flex justify-content-center align-items-center">
-          <i class="mdi mdi-spotify spotify"></i>
-        </div>
+  <div class="row text-light py-3 px-1">
+    <div class="col-3">
+      {{ track.albumImg }}
+    </div>
+    <div class="col-7">
+      <h5>
+        {{ track.name }}
+      </h5>
+      <div>
+        By Artist
       </div>
+    </div>
+    <div class="col-2 d-flex justify-content-center align-items-center">
+      <i class="mdi mdi-spotify spotify"></i>
     </div>
   </div>
 </template>
