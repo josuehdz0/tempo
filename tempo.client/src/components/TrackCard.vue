70c522a4389821f2531b2294b064aa41c8f03dad
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-03-13 15:37:58 -0600

    TrackCard, Track model

---
+++ tempo.client/src/components/TrackCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row text-light py-3 px-1">
+    <div class="col-3">
+      <img :src="playlist?.tracks[0]?.albumImg" alt="" class="img-fluid rounded">
+    </div>
+    <div class="col-7">
+      <h2>
+        {{ playlist?.name }}
+
+      </h2>
+      <div>
+        {{ playlist?.creator.spotify.display_name }}
+      </div>
+    </div>
+    <div class="col-2 d-flex justify-content-center align-items-center">
+      <i class="mdi mdi-heart heart"></i>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
