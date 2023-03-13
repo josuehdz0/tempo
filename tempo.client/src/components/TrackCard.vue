d29cdb7ded26109cb84dfebb0ebe70682b8ddb81
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-03-13 15:57:54 -0600

    track card

--- tempo.client/src/components/TrackCard.vue
+++ tempo.client/src/components/TrackCard.vue
@@ -1,26 +1,36 @@
 <template>
-  <div class="row text-light py-3 px-1">
-    <div class="col-3">
-      <img :src="playlist?.tracks[0]?.albumImg" alt="" class="img-fluid rounded">
-    </div>
-    <div class="col-7">
-      <h2>
-        {{ playlist?.name }}
-
-      </h2>
-      <div>
-        {{ playlist?.creator.spotify.display_name }}
+  <div class="row justify-content-center">
+    <div class="col-10 col-md-5 mt-4 songcardbg">
+      <div class="row text-light py-3 px-1">
+        <div class="col-3">
+          {{ track.albumImg }}
+        </div>
+        <div class="col-7">
+          <h5>
+            {{ track.name }}
+          </h5>
+          <div>
+            By Artist
+          </div>
+        </div>
+        <div class="col-2 d-flex justify-content-center align-items-center">
+          <i class="mdi mdi-spotify spotify"></i>
+        </div>
       </div>
-    </div>
-    <div class="col-2 d-flex justify-content-center align-items-center">
-      <i class="mdi mdi-heart heart"></i>
     </div>
   </div>
 </template>
 
 
 <script>
+import { Track } from "../models/Track";
+
 export default {
+
+  props: {
+    track: { type: Track, required: true }
+  },
+
   setup() {
     return {}
   }
