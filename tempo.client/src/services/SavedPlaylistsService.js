036522a65b4964091db0ef714fbafd1bc5c64e5c
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-03-14 10:04:57 -0600

    savedPlaylist written in service. doesn't save to appstate yet

--- tempo.client/src/services/SavedPlaylistsService.js
+++ tempo.client/src/services/SavedPlaylistsService.js
@@ -1,4 +1,11 @@
+import { logger } from "../utils/Logger"
+
 class SavedPlaylistsService {
+
+  async savePlaylist(playlistData){
+    const res = await api.post('api/savedPlaylists', playlistData)
+    logger.log("[SAVING PLAYLIST]", res.data)
+  }
 
 }
 
