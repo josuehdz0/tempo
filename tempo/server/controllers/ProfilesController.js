c001879db07ef0dba4b1534828cce0665bddbe8f
Hayden Liles <lileshaydenreal@gmail.com>
2023-03-14 14:15:11 -0600

    updated get my playlists

--- tempo/server/controllers/ProfilesController.js
+++ tempo/server/controllers/ProfilesController.js
@@ -11,7 +11,7 @@ export class ProfilesController extends BaseController {
       .get('', this.getProfiles)
       .get('/:id', this.getProfile)
       .use(Auth0Provider.getAuthorizedUserInfo)
-      .get('', this.getMyPlaylists)
+      .get('/spotify', this.getMyPlaylists)
   }
 
   async getMyPlaylists(req, res, next) {
